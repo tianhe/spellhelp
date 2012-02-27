6c1c300e13296f6335cf07f2048cb0932ff92a9c
Tian He <tian.he@gojee.com>
2012-02-26 22:27:01 -0500

    intial commit

---
+++ spellhelper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spellhelper::Application.config.secret_token = 'c970f4304e3660676931c495873b2886eecaec1a6dd4ad5de4754ce056085ffb2ccbe8f0189075ae03e7ff738434a04b4164a50a01e2812b1c9a6c5b0e9939d2'
